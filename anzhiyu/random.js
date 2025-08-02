5e888af9fdf0b1033a0194b56d47402c6f00c7c2
Nightliuguoxing <2050180797@qq.com>
2025-08-02 17:17:27 +0800

    BY LGX

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["20250802/Y001/"];function toRandomPost(){
+var posts=["20220803/Y001/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
