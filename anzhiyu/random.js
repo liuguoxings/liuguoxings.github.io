3bf9ffa83d5142219d6ce0d8ee5a9708332b38eb
Nightliuguoxing <2050180797@qq.com>
2025-08-05 11:42:37 +0800

    BY LGX

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["GO/G04/","GO/G02/","GO/G03/","GO/G05/","JAVA/J02/","Matlab/M00/","JAVA/J01/","JAVA/J04/","JAVA/J03/","Matlab/M01/","GO/G01/","Matlab/M03/","Python/P001/","Python/P003/","Python/P002/","Python/P004/","Matlab/M02/","YOLO/Y001/","Matlab/M04/","Matlab/M06/"];function toRandomPost(){
+var posts=["GO/G01/","GO/G02/","Matlab/M00/","Matlab/M01/","GO/G05/","GO/G04/","GO/G03/","Matlab/M02/","Matlab/M04/","Matlab/M03/","JAVA/J02/","JAVA/J01/","Matlab/M06/","JAVA/J04/","YOLO/Y001/","JAVA/J03/","Python/P002/","Python/P001/","Python/P003/","Python/P004/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
